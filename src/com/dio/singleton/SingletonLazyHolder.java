4e68597200ae88d835a85f4bf58ded16d392f5c9
José Pinehiro <josepsousa2012@gmail.com>
2022-12-05 00:54:50 -0300

    First

---
+++ src/com/dio/singleton/SingletonLazyHolder.java
@@ -0,0 +1,16 @@
+package com.dio.singleton;
+
+public class SingletonLazyHolder {
+	
+	private static class Holder {
+		public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+	}
+	
+	private SingletonLazyHolder() {
+		super();
+	}
+	
+	public static SingletonLazyHolder getInstancia() {
+		return Holder.instancia;
+	}
+}
